7915e9c464d699784771cda1443f23b3c93ccde3
Dimitar Minkov <minnkov@gmail.com>
2019-05-14 17:44:20 +0300

    Removed Playground.java files

--- Playground2.java
+++
@@ -1,9 +0,0 @@
-public class Playground2 {
-    public static void main(String[] args) {
-
-        for (int i = 0; i < args.length; i++) {
-
-            System.out.println(i);
-        }
-    }
-}
